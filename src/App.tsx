e2d957460cb0e4e070e4048610a0fa88052fde47
kyorohiro <kyorohiro@gmail.com>
2026-04-17 03:29:24 +0900

    add back button

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,8 @@ export default function App() {
           <div className="pointer-events-auto flex gap-2 rounded-2xl border border-white/10 bg-black/30 p-2 backdrop-blur">
             <button
               className={`rounded-xl px-3 py-2 text-sm ${language === "ja"
-                  ? "bg-blue-500 text-white"
-                  : "bg-white/10 text-slate-200 hover:bg-white/20"
+                ? "bg-blue-500 text-white"
+                : "bg-white/10 text-slate-200 hover:bg-white/20"
                 }`}
               onClick={() => setLanguage("ja")}
             >
@@ -24,12 +24,22 @@ export default function App() {
             </button>
             <button
               className={`rounded-xl px-3 py-2 text-sm ${language === "en"
-                  ? "bg-blue-500 text-white"
-                  : "bg-white/10 text-slate-200 hover:bg-white/20"
+                ? "bg-blue-500 text-white"
+                : "bg-white/10 text-slate-200 hover:bg-white/20"
                 }`}
               onClick={() => setLanguage("en")}
             >
               English
+            </button>
+          </div>
+        </div>
+        <div className="flex items-start justify-start p-4 top-1 absolute">
+          <div className="pointer-events-auto flex gap-2 rounded-2xl border border-white/10 bg-black/30 p-2 backdrop-blur">
+            <button
+              className={`rounded-xl px-3 py-2 text-sm bg-white/10 text-slate-200 hover:bg-white/20`}
+              onClick={() => setLanguage("ja")}
+            >
+              Back
             </button>
           </div>
         </div>
