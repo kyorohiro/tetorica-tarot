73f3df2bca1b8ef91a2e4792e129f93ef3112535
kyorohiro <kyorohiro@gmail.com>
2026-04-19 21:49:25 +0900

    update demo2

--- docs/demo2/assets/BufferResource-D-A2PseS.js
+++ docs/demo2/assets/BufferResource-D-A2PseS.js
@@ -1,4 +1,4 @@
-import{w as U,p as g,S as c,u as S,B as _,q as m,E as I,r as h}from"./index-BpZ8FVAl.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,p as g,S as c,u as S,B as _,q as m,E as I,r as h}from"./index-ocrM8KDZ.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
